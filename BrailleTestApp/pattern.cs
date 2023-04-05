ad03ff143ebe96abd47d8008ee7b3d32a25e6ede
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2023-04-05 09:41:33 +0530

    Add files via upload
    Added pattern.cs

---
+++ BrailleTestApp/pattern.cs
@@ -0,0 +1,16 @@
+﻿namespace BrailleTestApp
+{
+    public class pattern
+    {
+        public string GeneratePattern(string name)
+        {
+            if (name == "A")
+                return ".";
+            if (name == "B")
+                return "..";
+            else
+                return "done";
+
+        }
+    }
+}
