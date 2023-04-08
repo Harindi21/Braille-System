c75745f636475f15403833349dfdbf8679476d83
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2023-04-08 19:58:20 +0530

    Add files via upload
    Added a new js file

---
+++ BrailleTestApp/wwwroot/js/shapeParamters.js
@@ -0,0 +1,30 @@
+﻿    function activities()
+    {
+        var shape = document.getElementById("shapeForm").value;
+
+    // Hide all input fields by default
+    var inputs = document.querySelectorAll(".form-control");
+        for (var i = 0; i < inputs.length; i++)
+        {
+        inputs[i].style.display = "none";
+        }
+
+    // Show only the input fields relevant to the selected shape
+        if (shape === "Rectangle")
+        {
+            document.getElementById("RectangleHeight").style.display = "block";
+            document.getElementById("RectangleLength").style.display = "block";
+        }
+        else if (shape === "Circle")
+        {
+            document.getElementById("CircleRadius").style.display = "block";
+        }
+        else
+        {
+            document.getElementById("SquareLength").style.display = "block";
+        }
+    }
+
+    
+
+
