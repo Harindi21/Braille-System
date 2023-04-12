88888b1ef6a087982d107e3f06e15d1abf0a23e5
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2023-04-12 08:47:17 +0530

    Changed the ApplicationDbContext

--- BrailleTestApp/Data/ApplicationDbContext.cs
+++ BrailleTestApp/Data/ApplicationDbContext.cs
@@ -1,15 +1,41 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BrailleTestApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using BrailleTestApp.Models;
 
 namespace BrailleTestApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    //enter the full code here deleted the existing code (3)
+    public partial class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-        public DbSet<BrailleTestApp.Models.Braille>? Braille { get; set; }
+        public DbSet<Models.Braille>? Braille { get; set; }
+
+        //added this because of chat
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+        }
     }
+
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(255);
+            builder.Property(u => u.FirstName).HasMaxLength(255);
+        }
+    }
+
 }
--- BrailleTestApp/Data/ApplicationUser.cs
+++ BrailleTestApp/Data/ApplicationUser.cs
@@ -1,6 +1,16 @@
-﻿namespace BrailleTestApp.Data
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BrailleTestApp.Data
 {
-    public class ApplicationUser
+    //added an inheritence (1st step)
+    //added the properties I need  (2)
+    public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
