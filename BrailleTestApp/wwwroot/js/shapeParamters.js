11db81b6bf7934d89fa2f3db903ea1bcc3d1c5ce
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2023-04-13 16:54:03 +0530

    Update shapeParamters.js
    Added new js code to generate new features in form controls

--- BrailleTestApp/wwwroot/js/shapeParamters.js
+++ BrailleTestApp/wwwroot/js/shapeParamters.js
@@ -1,29 +1,230 @@
-﻿    function activities()
-    {
-        var shape = document.getElementById("shapeForm").value;
+function showHideFields() {
 
-    // Hide all input fields by default
-    var inputs = document.querySelectorAll(".form-control");
-        for (var i = 0; i < inputs.length; i++)
-        {
-        inputs[i].style.display = "none";
-        }
+    var shape = document.getElementById("myDropDownList1").value;
 
-    // Show only the input fields relevant to the selected shape
-        if (shape === "Rectangle")
-        {
-            document.getElementById("RectangleHeight").style.display = "block";
-            document.getElementById("RectangleLength").style.display = "block";
-        }
-        else if (shape === "Circle")
-        {
-            document.getElementById("CircleRadius").style.display = "block";
-        }
-        else
-        {
-            document.getElementById("SquareLength").style.display = "block";
-        }
+    if (shape == "Circle") {
+        document.getElementById("CircleDiv").style.display = "block";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
     }
+    else if (shape == "Square") {
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("SquareDiv").style.display = "block";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Rectangle") {
+        document.getElementById("RectangleDiv").style.display = "block";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Triangle") {
+        document.getElementById("TriangleDiv").style.display = "block";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Pentagon") {
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "block";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Hexagon") {
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "block";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Heptagon") {
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "block";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Octagon") {
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "block";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Nonagon") {
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "block";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Parallelogram") {
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "block";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Trapezoid") {
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "block";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Rhombus") {
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "block";
+        document.getElementById("QuadrilateralDiv").style.display = "none";
+
+    }
+    else if (shape == "Quadrilateral") {
+        document.getElementById("CircleDiv").style.display = "none";
+        document.getElementById("SquareDiv").style.display = "none";
+        document.getElementById("EnterDiv").style.display = "block";
+        document.getElementById("RectangleDiv").style.display = "none";
+        document.getElementById("TriangleDiv").style.display = "none";
+        document.getElementById("PentagonDiv").style.display = "none";
+        document.getElementById("HexagonDiv").style.display = "none";
+        document.getElementById("HeptagonDiv").style.display = "none";
+        document.getElementById("OctagonDiv").style.display = "none";
+        document.getElementById("NonagonDiv").style.display = "none";
+        document.getElementById("ParallelogramDiv").style.display = "none";
+        document.getElementById("TrapezoidDiv").style.display = "none";
+        document.getElementById("RhombusDiv").style.display = "none";
+        document.getElementById("QuadrilateralDiv").style.display = "block";
+
+    }
+
+}
 
     
 
