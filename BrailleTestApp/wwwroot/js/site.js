21fbeeeb91444914e83893c483680fa2fdff3e0d
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2023-04-16 20:42:37 +0530

    added the print preview using javascript.

--- BrailleTestApp/wwwroot/js/site.js
+++ BrailleTestApp/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function printPreview() {
+    window.print();
+    alert("Successfully Sent! Document will be printing soon.");
+}
